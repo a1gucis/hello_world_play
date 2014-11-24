5f669d65eda75d189e8aab2f2c9dd9cf4fe5c3f4
a1gucis <a1gucis@a1guciss-MacBook-Air.local>
2014-11-24 21:34:15 +0200

    Product model has id String instead of ean Long

--- app/controllers/ProductsController.scala
+++ app/controllers/ProductsController.scala
@@ -4,17 +4,18 @@ import models.Product
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import play.api.mvc.{Action, Controller}
+import java.util.UUID
 
 
 object ProductsController extends Controller {
   implicit val productWrites: Writes[Product] = (
-    (JsPath \ "ean").write[Long] and
+    (JsPath \ "id").write[String] and
       (JsPath \ "name").write[String] and
       (JsPath \ "description").write[String]
     )(unlift(Product.unapply))
 
   implicit val productReads: Reads[Product] = (
-    (JsPath \ "ean").read[Long] and
+    (JsPath \ "id").read[String] and
       (JsPath \ "name").read[String] and
       (JsPath \ "description").read[String]
     )(Product.apply _)
@@ -24,7 +25,7 @@ object ProductsController extends Controller {
   }
 
   def post = Action(parse.json) { request =>
-    val productJson = request.body.asInstanceOf[JsObject] ++ Json.obj("ean" -> JsNumber(Product.maxId + 1))
+    val productJson = request.body.asInstanceOf[JsObject] ++ Json.obj("id" -> JsString(UUID.randomUUID.toString))
     val product = productJson.as[Product]
     try {
       Product.insert(product)
@@ -35,15 +36,15 @@ object ProductsController extends Controller {
     }
   }
 
-  def getOne(ean: Long) = Action {
-    Product.findByEan(ean) match {
+  def getOne(id: String) = Action {
+    Product.findById(id) match {
       case Some(product) => Ok(Json.toJson(product))
       case None => NotFound
     }
   }
 
-  def putOne(ean: Long) = Action(parse.json) { request =>
-    val productJson = request.body.asInstanceOf[JsObject] ++ Json.obj("ean" -> JsNumber(ean))
+  def putOne(id: String) = Action(parse.json) { request =>
+    val productJson = request.body.asInstanceOf[JsObject] ++ Json.obj("id" -> JsString(id))
     val product = productJson.as[Product]
     try {
       Product.save(product)
